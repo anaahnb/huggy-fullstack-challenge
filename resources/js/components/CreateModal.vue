dffa5d1e8bcb1bfeafdf24d2950e4a8dd4d8f682
Ana Júlia <82275552+anaahnb@users.noreply.github.com>
2024-03-31 16:39:25 -0300

    CreateModal with success and errors toast alert

--- resources/js/components/CreateModal.vue
+++ resources/js/components/CreateModal.vue
@@ -8,17 +8,17 @@
 
             <template v-slot:body>
                 <div>   
-                    <input-component v-model="newContact.contatos_nome" label="Nome" placeholder="Nome completo"></input-component>
-                    <input-component typeInput="email" v-model="newContact.contatos_email" label="Email" placeholder="Email"></input-component>
+                    <input-component v-model="newContact.contatos_nome" label="Nome" placeholder="Nome completo" :error-message="errors.contatos_nome && errors.contatos_nome[0]" :typeStyle="errors.contatos_nome ? 'danger' : 'default'"></input-component>
+                    <input-component typeInput="email" v-model="newContact.contatos_email" label="Email" placeholder="Email" :errorMessage="errors.contatos_email && errors.contatos_email[0]"></input-component>
                     <input-component typeInput="file" @change="handleFileUpload" label="Imagem"></input-component>
                     <div class="input__row">
-                        <input-component typeInput="number" v-model="newContact.contatos_telefone" label="Telefone" placeholder="Telefone"></input-component>
+                        <input-component typeInput="number" v-model="newContact.contatos_telefone" label="Telefone" placeholder="Telefone" :errorMessage="errors.contatos_telefone && errors.contatos_telefone[0]" :typeStyle="errors.contatos_telefone ? 'danger' : 'default'"></input-component>
                         <input-component typeInput="number" v-model="newContact.contatos_celular" label="Celular" placeholder="Celular"></input-component>
                     </div>
-                    <input-component v-model="newContact.endereco_rua" label="Endereço" placeholder="Endereço"></input-component>
+                    <input-component v-model="newContact.endereco_rua" label="Endereço" placeholder="Endereço" :errorMessage="errors.endereco_rua && errors.endereco_rua[0]" :typeStyle="errors.endereco_rua ? 'danger' : 'default'"></input-component>
                     <div class="input__row">
-                        <input-component v-model="newContact.cidade_id" label="Cidade" placeholder="Cidade"></input-component>
-                        <input-component v-model="newContact.endereco_bairro" label="Bairro" placeholder="Bairro"></input-component>
+                        <input-component v-model="newContact.cidade_id" label="Cidade" placeholder="Cidade" :errorMessage="errors.cidade_id && errors.cidade_id[0]" :typeStyle="errors.cidade_id ? 'danger' : 'default'"></input-component>
+                        <input-component v-model="newContact.endereco_bairro" label="Bairro" placeholder="Bairro" :errorMessage="errors.endereco_bairro && errors.endereco_bairro[0]" :typeStyle="errors.endereco_bairro ? 'danger' : 'default'"></input-component>
                     </div>
                 </div>
             </template>
@@ -36,13 +36,11 @@
     import { ref } from 'vue';
     import { useToast } from 'vue-toastification';
 
-
     import ModalComponent from '/resources/js/components/ModalComponent.vue'
     import InputComponent from '/resources/js/components/InputComponent.vue'
     import ButtonComponent from '/resources/js/components/ButtonComponent.vue'
 
     import useContacts from '/resources/js/composables/contatos.js';
-
 
     export default {
         name: "CreateModal",
@@ -52,7 +50,8 @@
             ButtonComponent
         },
         setup(props, { emit } ) {
-            const { createContact } = useContacts();
+            const { createContact, errors } = useContacts();
+
             const toast = useToast();
 
             const newContact = ref({
@@ -72,21 +71,29 @@
 
             const submitCreateContactForm = async () => {
                 try {
-                    const formData = new FormData();
-                    for (const key in newContact.value) {
-                        formData.append(key, newContact.value[key]);
+                    if (errors) {
+                        toast.error("Por favor, preencha todos os campos do formulário.");
+                        return; 
+                    } else {
+                        const formData = new FormData();
+                        for (const key in newContact.value) {
+                            formData.append(key, newContact.value[key]);
+                        }
+                        await createContact(formData);
+                        toast.success("Contato criado com sucesso!", { timeout: 2000 });
+                        emit('close');
                     }
-                    await createContact(formData);
-                    toast.success("Contato criado com sucesso!", { timeout: 2000 });
-                    emit('close');
+
                 } catch (error) {
                     console.error(error);
                 }
             };
 
+
             return {
                 newContact,
                 toast,
+                errors,
                 submitCreateContactForm,
                 handleFileUpload,
             };
