0e387e302fddac5f3ecbcdc66fbeeeb94e150acd
Ana Júlia <82275552+anaahnb@users.noreply.github.com>
2024-04-03 17:29:31 -0300

    feat: add logic to generate contact image based on name in ContatoSeeder seeder

--- database/seeders/ContatoSeeder.php
+++ database/seeders/ContatoSeeder.php
@@ -7,26 +7,40 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as FakerFactory;
 
-
 class ContatoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
-{
-    $faker = FakerFactory::create();
+    {
+        $faker = FakerFactory::create();
 
-    for ($i = 0; $i < 5; $i++) {
-        Contato::create([
-            'contatos_nome' => $faker->name,
-            'contatos_email' => $faker->unique()->safeEmail,
-            'contatos_telefone' => $faker->numerify('##########'),
-            'contatos_celular' => $faker->numerify('##########'), 
-            'endereco_id' => 1,
-            'contatos_imagem' => $faker->lexify('??')
-        ]);
+        for ($i = 0; $i < 10; $i++) {
+            $contatoNome = $faker->name;
+            $contatoImagem = $this->generateContatoImagem($contatoNome);
+
+            Contato::create([
+                'contatos_nome' => $contatoNome,
+                'contatos_email' => $faker->unique()->safeEmail,
+                'contatos_telefone' => $faker->numerify('##########'),
+                'contatos_celular' => $faker->numerify('##########'), 
+                'endereco_id' => 1,
+                'contatos_imagem' => $contatoImagem
+            ]);
+        }
     }
-}
 
+    /**
+     * Gerar imagem de contato baseado no nome.
+     *
+     * @param string $contatoNome
+     * @return string
+     */
+    private function generateContatoImagem(string $contatoNome): string
+    {
+        $text = explode(" ", $contatoNome);
+        $image = isset($text[1]) ? substr($text[0], 0, 1) . substr($text[1], 0, 1) : substr($text[0], 0, 2);
+        return $image;
+    }
 }
